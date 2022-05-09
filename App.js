32e0abac0fb74fb9f3a710de7986b966cc827eeb
aarvis <aakashjarvis1@gmail.com>
2022-05-10 00:17:03 +0530

    just the beginning

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {DARK} from './Theme/Theme';
+
+const App = () => {
+  return (
+    <View style={{width:'100%', height:'100%', backgroundColor:DARK.BACKGROUND_COLOR}}>
+    </View>
+  );
+};
+
+
+
+export default App;
